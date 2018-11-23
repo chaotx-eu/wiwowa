5f397835142d2a3d57fc6d412e5015cd7d549af1
hdainester <27729552+hdainester@users.noreply.github.com>
2018-11-23 16:02:29 +0100

    Initial commit - set up pom.xml (maven) - set up ide (vscode) - added some empty classes

---
+++ src/main/java/eu/zoho/chaotx/app/App.java
@@ -0,0 +1,21 @@
+package eu.zoho.chaotx.app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Application for managing and booking and user
+ * data in its own projects
+ * 
+ * @Copyright Chaotx-EU
+ */
+public class App extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage unused) {
+        
+    }
+}
